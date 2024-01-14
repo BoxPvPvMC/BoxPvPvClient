54e4b95126ac5b40320d88c9cc951f12cc23eb9f
Game_Time <108236317+RayBytes@users.noreply.github.com>
2024-01-14 21:18:06 +0500

    make changelogs system work

--- app/assets/js/scripts/settings.js
+++ app/assets/js/scripts/settings.js
@@ -1453,7 +1453,7 @@ function populateAboutVersionInformation(){
  */
 function populateReleaseNotes(){
     $.ajax({
-        url: 'https://github.com/BoxPvPvClient/BoxPvPvClient/releases.atom',
+        url: 'https://github.com/BoxPvPvMC/BoxPvPvClient/releases.atom',
         success: (data) => {
             const version = 'v' + remote.app.getVersion()
             const entries = $(data).find('entry')
