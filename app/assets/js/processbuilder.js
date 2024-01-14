6b44deb2e1581ba66e8af04b99f20aae84ac026c
Game_Time <108236317+RayBytes@users.noreply.github.com>
2024-01-14 16:46:05 +0500

    this is a commit message

--- app/assets/js/processbuilder.js
+++ app/assets/js/processbuilder.js
@@ -521,7 +521,7 @@ class ProcessBuilder {
                             val = args[i].replace(argDiscovery, tempNativePath)
                             break
                         case 'launcher_name':
-                            val = args[i].replace(argDiscovery, 'BoxPvPv-Launcher')
+                            val = args[i].replace(argDiscovery, 'BoxPvPv_Launcher')
                             break
                         case 'launcher_version':
                             val = args[i].replace(argDiscovery, this.launcherVersion)
--- app/assets/js/scripts/settings.js
+++ app/assets/js/scripts/settings.js
@@ -1453,7 +1453,7 @@ function populateAboutVersionInformation(){
  */
 function populateReleaseNotes(){
     $.ajax({
-        url: 'https://github.com/dscalzi/HeliosLauncher/releases.atom',
+        url: 'https://github.com/BoxPvPvClient/BoxPvPvClient/releases.atom',
         success: (data) => {
             const version = 'v' + remote.app.getVersion()
             const entries = $(data).find('entry')
